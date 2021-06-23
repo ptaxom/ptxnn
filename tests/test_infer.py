0082d3225404e810e7ede40d6681ce689e4f27d2
Pavel Khursov <pkhursov@biquants.com>
2021-06-24 01:53:26 +0300

    Batched prediction in test_infer

--- tests/test_infer.py
+++ tests/test_infer.py
@@ -19,7 +19,7 @@ parser.add_argument('-p', '--path', dest='path', action='store',
 parser.add_argument('-d', '--dim', dest='dim', action='store',
                     required=False, type=int, default=256,
                     help='Input dim, in case of square form')
-parser.add_argument('-b', '--batch_size', dest='batchsize', action='store',
+parser.add_argument('-b', '--batchsize', dest='batchsize', action='store',
                     required=False, type=int, default=1,
                     help='Input dim, in case of square form')
 parser.add_argument('-i', '--input', dest='input_image', action='store',
@@ -31,12 +31,11 @@ image = cv2.imread(args.input_image)
 if image is None:
     raise RuntimeError(f'Couldnt load image {args.input_image}')
 
+mcdnn.set_severity(mcdnn.kVERBOSE)
 engine = mcdnn.GeneralInferenceEngine(args.model, args.path)
 
 sz = args.dim
 resize_img = cv2.resize(image, (sz, sz), interpolation=cv2.INTER_CUBIC)
 type_img = resize_img.astype("float32").transpose(2, 0, 1)[np.newaxis]  # (1, 3, h, w)
-
-predict = engine.predict(type_img)
-print(predict)
-print(np.sum(predict[0] ** 2))
+input_data = np.concatenate([type_img for _ in range(args.batchsize)], axis=0)
+predict = engine.predict(input_data)
